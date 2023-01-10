ce9a80c748b7417e59c49fd3769e4792705b2f25
Maheer <86462580+okmaheer@users.noreply.github.com>
2023-01-10 17:19:08 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
 import Home from "./routes/home/home.component.jsx";
 import { Route, Routes } from "react-router-dom";
+import Navigation from "./routes/navigation/navigation.component.jsx";
+
+const Shop = () =>{
+  return (
+    <h1>I am the hell King</h1>
+  );
+}
 const App = () => {
 
   return (
     <Routes>
-      <Route path="/" element={<Home />}/>
+      <Route path="/" element={<Navigation />}>
+      <Route index element={<Home />}/>
+      <Route path='/shop' element={<Shop/>}/>
+      </Route>
+
+      
     </Routes>
 
   );
 };
 
 export default App;
+ 
