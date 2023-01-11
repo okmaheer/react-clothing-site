5e46656319b47b18a942012ce0199f51e6dafe9f
Maheer <86462580+okmaheer@users.noreply.github.com>
2023-01-11 17:24:02 +0500

    Update home.component.jsx

--- src/routes/home/home.component.jsx
+++ src/routes/home/home.component.jsx
@@ -1,8 +1,14 @@
 import Directory from "../../component/directory/directory.component";
+import { Outlet } from "react-router-dom";
 const Home = () => {
 
+
   return (
+    <div>
+  
     <Directory/>
+    <Outlet/>
+    </div>
   );
 };
 
