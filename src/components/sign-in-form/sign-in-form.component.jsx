5813c30e1a4d20557921450ed435950b293143b1
Maheer <86462580+okmaheer@users.noreply.github.com>
2023-01-17 23:26:37 +0500

    DAY 17 COMPONENT ADDED

---
+++ src/components/sign-in-form/sign-in-form.component.jsx
@@ -0,0 +1,74 @@
+import { useState } from 'react';
+
+import FormInput from '../form-input/form-input.component';
+import Button from '../button/button.component';
+
+import {
+  createAuthUserWithEmailAndPassword,
+  createUserDocumentFromAuth,
+} from '../../utils/firebase/firebase.utils';
+
+import './sign-in-form.styles.scss';
+
+const defaultFormFields = {
+
+  email: '',
+  password: '',
+};
+
+const SignInForm = () => {
+  const [formFields, setFormFields] = useState(defaultFormFields);
+  const { email, password } = formFields;
+
+  const resetFormFields = () => {
+    setFormFields(defaultFormFields);
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+
+    try {
+      
+    } catch (error) {
+      
+    }
+  };
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+
+    setFormFields({ ...formFields, [name]: value });
+  };
+
+  return (
+    <div className='sign-up-container'>
+      <h2>Don't have an account?</h2>
+      <span>Sign up with your email and password</span>
+      <form onSubmit={handleSubmit}>
+
+        <FormInput
+          label='Email'
+          type='email'
+          required
+          onChange={handleChange}
+          name='email'
+          value={email}
+        />
+
+        <FormInput
+          label='Password'
+          type='password'
+          required
+          onChange={handleChange}
+          name='password'
+          value={password}
+        />
+
+        <Button type='submit'>Sign Up</Button>
+      </form>
+    </div>
+  );
+};
+
+export default SignInForm;
