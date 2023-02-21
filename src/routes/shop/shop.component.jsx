dffbb07f05e26c92ef21c0adf14925ee061abeb1
Maheer <86462580+okmaheer@users.noreply.github.com>
2023-02-21 19:57:19 +0500

    Update shop.component.jsx

--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -7,6 +7,7 @@ const Shop = () => {
   return (
     <div className='products-container'>
       {products.map((product) => (
+      
         <ProductCard key={product.id} product={product} />
       ))}
     </div> 
