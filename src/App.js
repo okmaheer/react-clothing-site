5b17c606397bfbaae75679a03dd13e5e28ee4950
Maheer <86462580+okmaheer@users.noreply.github.com>
2023-03-07 22:09:14 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ const App = () => {
   return (
     <Routes>
       <Route path='/' element={<Navigation />}>
-        <Route index element={<Home />} />
+        <Route index element={<Home />} /> 
         <Route path='shop' element={<Shop />} />
         <Route path='auth' element={<Authentication />} />
       </Route>
