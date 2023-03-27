2b1b0c5f32b86526836bdd7a2abc8d5c3c209d1e
Maheer <86462580+okmaheer@users.noreply.github.com>
2023-03-28 00:55:18 +0500

    Update directory.component.jsx

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -1,41 +1,15 @@
 import CategoryItem from '../category-item/category-item.component';
-const Directory = ()=>{
-    const categories = [
-        {
-          "id": 1,
-          "title": "hats",
-          "imageUrl": "https://i.ibb.co/cvpntL1/hats.png"
-        },
-        {
-          "id": 2,
-          "title": "jackets",
-          "imageUrl": "https://i.ibb.co/px2tCc3/jackets.png"
-        },
-        {
-          "id": 3,
-          "title": "sneakers",
-          "imageUrl": "https://i.ibb.co/0jqHpnp/sneakers.png"
-        },
-        {
-          "id": 4,
-          "title": "womens",
-          "imageUrl": "https://i.ibb.co/GCCdy8t/womens.png"
-        },
-        {
-          "id": 5,
-          "title": "mens",
-          "imageUrl": "https://i.ibb.co/R70vBrQ/men.png"
-        }
-      ]
-    return (
-        <div className='categories-container'>
-        {categories.map((category) => (
-         <CategoryItem key={category.id} category={category}  />
-        ))}
-  
-  
-      </div>
-    )}
 
+import './directory.styles.scss';
 
-    export default Directory;
+const Directory = ({ categories }) => {
+  return (
+    <div className='directory-container'>
+      {categories.map((category) => (
+        <CategoryItem key={category.id} category={category} />
+      ))}
+    </div>
+  );
+};
+
+export default Directory;
