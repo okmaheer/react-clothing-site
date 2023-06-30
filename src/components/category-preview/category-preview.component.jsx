f0314487200946a3ccdc6ad8d829a864b97a7068
Maheer <86462580+okmaheer@users.noreply.github.com>
2023-06-30 12:11:25 +0500

    changes

--- src/components/category-preview/category-preview.component.jsx
+++ src/components/category-preview/category-preview.component.jsx
@@ -1,17 +1,26 @@
 import ProductCard from '../product-card/product-card.component';
-import './category-preview.styles.scss';
-const CategoryPreview = ({ title, products }) => (
-  <div className='category-preview'>
-    <h2>
-      <span className='title'>{title.toUpperCase()}</span>
-    </h2>
-    <div className='preview'>
-      {products
-        .filter((_, idx) => idx < 4)
-        .map((product) => (
-          <ProductCard key={product.id} product={product} />
-        ))}
-    </div>
-  </div>
-);
+
+import {
+  CategoryPreviewContainer,
+  Title,
+  Preview,
+} from './category-preview.styles';
+
+const CategoryPreview = ({ title, products }) => {
+  return (
+    <CategoryPreviewContainer>
+      <h2>
+        <Title to={title}>{title.toUpperCase()}</Title>
+      </h2>
+      <Preview>
+        {products
+          .filter((_, idx) => idx < 4)
+          .map((product) => (
+            <ProductCard key={product.id} product={product} />
+          ))}
+      </Preview>
+    </CategoryPreviewContainer>
+  );
+};
+
 export default CategoryPreview;
